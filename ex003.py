ab3ff3a88bd7d62a585a04351c4fe9a5b13fa3b9
lfpbarros <lip.tjsba@gmail.com>
2022-10-16 07:19:47 -0300

    Exercícios 2 e 3

---
+++ ex003.py
@@ -0,0 +1,6 @@
+# Crie um programa que leia dois números e mostre a soma entre eles.
+
+valor_1 = int(input('Digite um valor: '))
+valor_2 = int(input('Digite outro valor: '))
+soma = valor_1 + valor_2
+print(f'A soma entre {valor_1} e {valor_2} é igual a {soma}!')
