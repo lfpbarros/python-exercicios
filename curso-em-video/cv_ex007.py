d70a364453e12f23861485d3c365d9ed11f852b0
Luis Felipe Barros <96773657+lfpbarros@users.noreply.github.com>
2022-10-17 09:22:47 -0300

    Adição do ex007

--- curso-em-video/cv_ex007.py
+++ curso-em-video/cv_ex007.py
@@ -3,4 +3,4 @@
 nota_1 = float(input('Primeira nota do aluno: '))
 nota_2 = float(input('Segunda nota do aluno: '))
 media = (nota_1 + nota_2) / 2
-print(f'A média entre {nota_1} e {nota_2} é igual a {media:.1f}.')
+print(f'A média entre {nota_1:.1f} e {nota_2:.1f} é igual a {media:.1f}.')
