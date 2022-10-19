6bf74db1ab7d18f250a5d023d27743a099ac3f46
lfpbarros <lip.tjsba@gmail.com>
2022-10-19 00:00:16 -0300

    Adição do ex019

---
+++ curso-em-video/cv_ex019.py
@@ -0,0 +1,12 @@
+# Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
+
+from random import choice
+
+primeiro = str(input('Primeiro aluno: '))
+segundo = str(input('Segundo aluno: '))
+terceiro = str(input('Terceiro aluno: '))
+quarto = str(input('Quarto aluno: '))
+
+lista = [primeiro, segundo, terceiro, quarto]
+
+print(f'O aluno escolhido foi {choice(lista)}')
