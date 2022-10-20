33b2dd388dd97f7a1e16826e71738cc22424fde1
lfpbarros <lip.tjsba@gmail.com>
2022-10-20 12:02:55 -0300

    Adição do ex033

---
+++ curso-em-video/cv_ex033.py
@@ -0,0 +1,29 @@
+# Faça um programa que leia três números e mostre qual é o maior e qual é o menor.
+
+
+n1 = int(input('Primeiro valor: '))
+n2 = int(input('Segundo valor: '))
+n3 = int(input('Terceiro valor: '))
+
+# Verificando quem é menor.
+if n1 < n2 and n1 < n3:
+    menor = n1
+
+if n2 < n1 and n2 < n3:
+    menor = n2
+
+if n3 < n1 and n3 < n2:
+    menor = n3
+
+#  Verificando quem é maior.
+if n1 > n2 and n1 > n3:
+    maior = n1
+
+if n2 > n1 and n2 > n3:
+    maior = n2
+
+if n3 > n1 and n3 > n2:
+    maior = n3
+
+print(f'O menor valor digitado foi {menor}')
+print(f'O maior valor digitado foi {maior}')
