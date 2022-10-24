0891f0894b2bf24d00a0fd9c0410e7a0522a9bba
Luis Felipe Barros <96773657+lfpbarros@users.noreply.github.com>
2022-10-24 11:42:06 -0300

    Adição do ex043

--- curso-em-video/cv_ex043.py
+++ curso-em-video/cv_ex043.py
@@ -8,7 +8,18 @@
 peso = float(input('Qual é o seu peso? (kg) '))
 altura = float(input('Qual é a sua altura? (m) '))
 
-# O IMC dessa pessoa é de {:.1f}
-# PARABÉNS, você está na faixa de PESO NORMAL
-# Você está em OBESIDADE!
-# Você está ABAIXO DO PESO normal!
+imc = peso / (altura ** 2)
+
+print(f'O IMC dessa pessoa é de {imc:.1f}')
+
+if imc < 18.5:
+    print('Você está ABAIXO DO PESO normal!')
+elif imc >= 18.5 and imc < 25:
+    print('PARABÉNS, você está na faixa de PESO NORMAL')
+elif imc >= 25 and imc < 30:
+    print('Você está com SOBREPESO')
+elif imc >= 30 and imc < 40:
+    print('Você está em OBESIDADE!')
+elif imc >= 40:
+    print('Você está com OBESIDADE MÓRBIDA, cuidado!')
+
