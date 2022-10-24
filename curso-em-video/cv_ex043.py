faaa307ae8d7211d592437d2e7c7f384e97d893b
lfpbarros <lip.tjsba@gmail.com>
2022-10-24 08:59:58 -0300

    Inicio do ex043

---
+++ curso-em-video/cv_ex043.py
@@ -0,0 +1,14 @@
+# Desenvolva uma lógica que leia o peso e a altura de uma pessoa, calcule seu IMC e mostre seu status, de acordo com a tabela abaixo:
+# Abaixo de 18.5: Abaixo do peso
+# Entre 18.5 e 25: Peso ideal
+# 25 até 30: Sobrepeso
+# 30 até 40: Obesidade
+# Acima de 40: Obesidade mórbida
+
+peso = float(input('Qual é o seu peso? (kg) '))
+altura = float(input('Qual é a sua altura? (m) '))
+
+# O IMC dessa pessoa é de {:.1f}
+# PARABÉNS, você está na faixa de PESO NORMAL
+# Você está em OBESIDADE!
+# Você está ABAIXO DO PESO normal!
