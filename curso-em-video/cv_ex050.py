5992d2c7fbb8b101bd703b6ef902e1fcf34d7a65
lfpbarros <lip.tjsba@gmail.com>
2022-11-08 12:15:37 -0300

    Adição do ex050

--- curso-em-video/cv_ex050.py
+++ curso-em-video/cv_ex050.py
@@ -1,1 +1,12 @@
 # Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o.
+
+soma = 0
+contador = 0
+
+for i in range(6):
+    n = int(input(f'Digite o {i + 1}º valor: '))
+    if n % 2 == 0:
+        soma += n
+        contador += 1
+
+print(f'Você informou {contador} números pares e soma deles é de {soma}')
