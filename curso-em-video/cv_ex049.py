fa6525e810d331df791909e2f9765397cffe5dc0
lfpbarros <lip.tjsba@gmail.com>
2022-11-08 12:01:37 -0300

    Adição do ex049

--- curso-em-video/cv_ex049.py
+++ curso-em-video/cv_ex049.py
@@ -1,1 +1,6 @@
 # Refaça o desafio 009, mostrando a tabuada de um número que o usuário escolher, só que agora utilizando o laço for.
+
+num = int(input('Digite um número para ver sua tabuada: '))
+
+for i in range(1, 11):
+    print(f'{num} x {i:2} = {num * i}')
