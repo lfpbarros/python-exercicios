16d6e3b9dc471caa7f8bf0454dd97007f78f6b3b
lfpbarros <lip.tjsba@gmail.com>
2022-11-08 11:48:00 -0300

    Atualização do ex047

--- curso-em-video/cv_ex047.py
+++ curso-em-video/cv_ex047.py
@@ -1,6 +1,7 @@
 # Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
 
 for i in range(1, 51):
+# Podemos diminuir o número de iterações por ir pulando de 2 em 2 range(2, 51, 2)
     if i % 2 == 0:
         print(i, end=', ')
 print('Acabou')
