bf329d6dc840b95fe007b67401e5f070764d3c36
lfpbarros <lip.tjsba@gmail.com>
2022-12-18 18:38:45 -0300

    Adição do ex051

--- curso-em-video/cv_ex051.py
+++ curso-em-video/cv_ex051.py
@@ -1,1 +1,17 @@
 # Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa progressão.
+
+print('=' * 25)
+print('{:^25}'.format('10 TERMOS DE UMA PA')) # o :^25 centraliza o valor nessa quantidade de espaços
+print('=' * 25)
+
+termo = int(input('Primeiro termo: '))
+razao = int(input('Razão: '))
+# décimo termo de uma pa: primeiro + (10 - 1) * razão
+
+print('{}'.format(termo), end=' -> ')
+
+for i in range(9):
+    termo += razao
+    print(f'{termo}', end=' -> ')
+
+print('ACABOU')
