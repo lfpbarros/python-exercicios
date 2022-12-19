eb185a08186206b4ac605b0040305bcc33b8256e
lfpbarros <lip.tjsba@gmail.com>
2022-12-19 08:39:51 -0300

    Adição do ex 052

--- curso-em-video/cv_ex052.py
+++ curso-em-video/cv_ex052.py
@@ -1,1 +1,16 @@
 # Faça um programa que leia um número inteiro e diga se ele é ou não um número primo.
+
+num = int(input('Insira um número: '))
+contador = 0
+
+for i in range(num - 1, 1, -1):
+    if num % i == 0:
+        contador += 1
+
+if num == 1 or num == 0 or num < 0:
+    contador += 1
+
+if contador == 0:
+    print(f'O número {num} é primo')
+else:
+    print(f'O número {num} não é primo')
